0f8e397abf2cff2025d1bc736da25fad8b1284ae
aabds <wogus8880@gmail.com>
2023-05-09 16:31:07 +0900

    0509

--- src/main/java/kr/ac/kopo/guestbook/repository/GuestbookRepository.java
+++ src/main/java/kr/ac/kopo/guestbook/repository/GuestbookRepository.java
@@ -1,6 +1,12 @@
 package kr.ac.kopo.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import kr.ac.kopo.guestbook.entity.Guestbook;
+import kr.ac.kopo.guestbook.entity.QGuestbook;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
--- src/test/java/kr/ac/kopo/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/kr/ac/kopo/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,16 +1,25 @@
 package kr.ac.kopo.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import kr.ac.kopo.guestbook.entity.Guestbook;
+import kr.ac.kopo.guestbook.entity.QGuestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class GuestbookRepositoryTests {
     @Autowired
     private GuestbookRepository guestbookRepository;
+
 
     @Test
     public void insertDummies() {
@@ -23,4 +32,52 @@ public class GuestbookRepositoryTests {
             System.out.println(guestbookRepository.save(guestbook));
         });
     }
+
+    @Test
+    public void updateTest(){
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+            guestbook.changeTitle("Changed Title...");
+            guestbook.changeContent("Changed Content...");
+            guestbookRepository.save(guestbook);
+        }
+    }
+
+
+    //단일 검색
+    @Test
+    public void testQuery1(){
+        Pageable pageable = PageRequest.of(0,10,Sort.by("gno").descending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "7";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
+
+    //다중검색
+    @Test
+    public void testQuery2(){
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").ascending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "7";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression expWriter = qGuestbook.writter.contains(keyword);
+        BooleanExpression expAll = expTitle.and(expWriter);
+        builder.and(expAll);
+        builder.and(qGuestbook.gno.gt(50L));
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
 }
