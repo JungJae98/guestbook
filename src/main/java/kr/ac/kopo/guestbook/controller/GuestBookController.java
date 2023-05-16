ad1b09a31d89c023f60911b9a2fbf654ccc88c34
aabds <wogus8880@gmail.com>
2023-05-16 15:31:04 +0900

    0516

--- src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
@@ -1,21 +1,31 @@
 package kr.ac.kopo.guestbook.controller;
 
+import kr.ac.kopo.guestbook.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook.dto.PageResultDTO;
+import kr.ac.kopo.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
-
+@RequiredArgsConstructor
 
 public class GuestBookController {
+    private final GuestbookService service;
 
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
 
-    @GetMapping({"/", "/list"})
-    public String list(){
-        log.info("방명록 확인");
-        return "/guestbook/list";
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list........." + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
