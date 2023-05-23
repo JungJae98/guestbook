a8c7ecb231a680ec3612371b17711359aa31afdb
aabds <wogus8880@gmail.com>
2023-05-23 16:34:59 +0900

    0523

--- src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.guestbook.controller;
 
+import kr.ac.kopo.guestbook.dto.GuestbookDTO;
 import kr.ac.kopo.guestbook.dto.PageRequestDTO;
 import kr.ac.kopo.guestbook.dto.PageResultDTO;
 import kr.ac.kopo.guestbook.service.GuestbookService;
@@ -8,7 +9,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -27,5 +30,20 @@ public class GuestBookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list........." + pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+    
+    // 등록처리 후 목록페이지로 이동
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("등록처리 후 목록페이지 리다이렉트" + dto);
+
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
     }
 }
